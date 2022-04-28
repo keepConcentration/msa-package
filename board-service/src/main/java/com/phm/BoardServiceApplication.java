87ddc50b66bd17dbd353fe8a5b00d7ebb95ec843
keepConcentration <phm543@gmail.com>
2022-04-28 20:13:40 +0900

    [create] user, board service application 추가

---
+++ board-service/src/main/java/com/phm/BoardServiceApplication.java
@@ -0,0 +1,15 @@
+package com.phm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class BoardServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardServiceApplication.class, args);
+	}
+
+}
